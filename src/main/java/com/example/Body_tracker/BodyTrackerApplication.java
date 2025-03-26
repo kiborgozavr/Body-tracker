6832c1074c41faa3d5d60b091e6d12b2322b4e81
kiborgozavr <k.volianiuk@gmail.com>
2025-03-26 19:32:17 +0200

    Initial commit

---
+++ src/main/java/com/example/Body_tracker/BodyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Body_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyTrackerApplication.class, args);
+	}
+
+}
